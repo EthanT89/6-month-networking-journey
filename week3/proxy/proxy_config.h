05e3f1691ab6fa2314ff1e0bfcc54d96f915d4b9
EthanT89 <ethan.l.thornberg@gmail.com>
2026-02-09 14:27:38 -0800

    Add UDP Proxy server configuration and utility headers

---
+++ week3/proxy/proxy_config.h
@@ -0,0 +1,11 @@
+/*
+ * proxy_config.h -- configuration for the UDP Proxy server
+ */
+
+#ifndef PROXY_CONFIG_H
+#define PROXY_CONFIG_H
+
+// Standard Global Configuration
+#define PROXY_PORT 5050
+
+#endif
---
+++ week3/proxy/utils/proxy_utils.h
@@ -0,0 +1,12 @@
+/*
+ * proxy_utils.h -- helper utils for sending data in/out of the server while preserving sender intent
+ */
+
+#ifndef PROXY_UTILS_H
+#define PROXY_UTILS_H
+
+#include "../proxy_config.h"
+
+int send_proxy();
+
+#endif
