e79e75f96c67e965acc0afa7be1e39e3daba4c8a
EthanT89 <ethan.l.thornberg@gmail.com>
2026-02-23 09:32:04 -0800

    feat: add worker management utilities including creation, addition, removal, and retrieval functions

---
+++ month3/week1/utils/workers.c
@@ -0,0 +1,86 @@
+/*
+ * workers.c -- worker utilities for the server
+ */
+
+#include "./workers.h"
+
+struct Worker *create_empty_worker(){
+    struct Worker *worker = malloc(sizeof *worker);
+    worker->id = -1;
+    worker->jobs_completed = 0;
+    worker->next = NULL;
+    worker->status = W_READY;
+
+    return worker;
+}
+
+void add_worker(struct Workers *workers, struct Worker *worker){
+    if (worker == NULL){
+        return;
+    }
+
+    if (workers->count == 0){
+        workers->head = workers->tail = worker;
+        workers->count++;
+        return;
+    }
+
+    workers->tail->next = worker;
+    workers->tail = worker;
+    workers->count++;
+    return;
+}
+
+void remove_worker(struct Workers *workers, int worker_id){
+    if (workers->count == 0){
+        return;
+    }
+
+    struct Worker *res = workers->head;
+    struct Worker *prev = NULL;
+
+    if (workers->count == 1){
+        if (workers->head->id != worker_id) return;
+        free(workers->head);
+        workers->head = workers->tail = NULL;
+        workers->count--;
+        return;
+    }
+
+    for (res; res != NULL && res->id != worker_id; res = res->next){
+        prev = res;
+    }
+
+    if (res == NULL) return;
+
+    if (res == workers->head){
+        workers->head = res->next;
+    } else if (res == workers->tail){
+        workers->tail = prev;
+        prev->next = NULL;
+    } else {
+        prev->next = res->next;
+    }
+
+    free(res);
+    workers->count--;
+    return;
+}
+
+struct Worker *get_worker_by_id(struct Workers *workers, int worker_id){
+    if (workers->count != 0){
+        for (struct Worker *cur = workers->head; cur != NULL; cur = cur->next){
+            if (cur->id == worker_id) return cur;
+        }
+    }
+    return NULL;
+}
+
+struct Worker *get_available_worker(struct Workers *workers){
+    if (workers->count != 0){
+        for (struct Worker *cur = workers->head; cur != NULL; cur = cur->next){
+            if (cur->status == W_READY) return cur;
+        }
+    }
+    return NULL;
+}
---
+++ month3/week1/utils/workers.h
@@ -0,0 +1,65 @@
+/*
+ * workers.h -- worker management for server use
+ */
+
+#ifndef WORKERS_H
+#define WORKERS_H
+
+#include "../common.h"
+
+/*
+ * Worker -- struct for server managment of workers
+ *
+ * id -- unique worker id
+ * status -- current status of the worker (ready, busy, failure, etc.)
+ * jobs_completed -- total number of successful jobs completed by the worker
+ */
+struct Worker {
+    int id;
+    int status;
+    int jobs_completed;
+
+    struct Worker *next;
+};
+
+/*
+ * Workers -- SLL data structure for server management of multiple workers
+ *
+ * *head -- pointer to the first worker in the list
+ * *tail -- pointer to the last worker in the list
+ * count -- total number of workers
+ * available_workers -- total number of available workers
+ */
+struct Workers {
+    struct Worker *head;
+    struct Worker *tail;
+    int count;
+    int available_workers;
+};
+
+/*
+ * create_empty_worker() -- initialize a worker struct and return a pointer to it
+ */
+struct Worker *create_empty_worker();
+
+/*
+ * add_worker() -- add a worker to the SLL 
+ */
+void add_worker(struct Workers *workers, struct Worker *worker);
+
+/*
+ * remove_worker() -- remove a worker from the SLL given its ID
+ */
+void remove_worker(struct Workers *workers, int worker_id);
+
+/*
+ * get_worker_by_id() -- given an id, search through the SLL and return a pointer to the corresponding worker, NULL if not found
+ */
+struct Worker *get_worker_by_id(struct Workers *workers, int worker_id);
+
+/*
+ * get_available_worker() -- search through the SLL and return a pointer to the first W_READY worker, NULL if not found
+ */
+struct Worker *get_available_worker(struct Workers *workers);
+
+#endif
