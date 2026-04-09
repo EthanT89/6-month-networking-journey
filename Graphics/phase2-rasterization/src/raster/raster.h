77214db520f5b82e2ecefbbbe0908f481457515b
EthanT89 <ethan.l.thornberg@gmail.com>
2026-04-08 17:19:37 -0700

    feat: implement triangle rasterization function with barycentric coordinates

--- Graphics/phase2-rasterization/src/raster/raster.h
+++ Graphics/phase2-rasterization/src/raster/raster.h
@@ -1,11 +1,20 @@
 #ifndef RASTER_H
 #define RASTER_H
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 #include "../framebuffer/framebuffer.h"
+#include "../../../phase1-math/src/vec/vector.h"
+#include "../geometry/geometry.h"
+#include <math.h>
 
 void draw_line(struct Framebuffer *fb, 
                int x0, int y0, int x1, int y1,
                unsigned char r, unsigned char g, unsigned char b);
+
+void draw_triangle(struct Framebuffer *fb, 
+                   struct Vector3 v0, struct Vector3 v1, struct Vector3 v2,
+                   unsigned char r, unsigned char g, unsigned char b);
 
 #endif
