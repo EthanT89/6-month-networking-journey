88442af32f0fcc796c1f7f3708f8df735e3c9e79
EthanT89 <ethan.l.thornberg@gmail.com>
2026-04-10 11:19:21 -0700

    feat: implement triangle geometry functions for back-face determination and area calculation

---
+++ Graphics/phase3-lighting/src/geometry/geometry.h
@@ -0,0 +1,20 @@
+#ifndef GEOMETRY2_H
+#define GEOMETRY2_H
+
+#include "../../../phase1-math/src/vec/vector.h"
+
+
+struct Triangle {
+    struct Vector3 v[3];
+    struct Vector3 normal;     // face normal in world space
+    float brightness[3];
+    unsigned char r, g, b;
+};
+
+int is_back_face(struct Triangle *t); // returns 1 if back-facing
+
+float signed_area(struct Vector3 A, struct Vector3 B, struct Vector3 C );
+
+struct Vector3 get_triangle_midpoint(struct Triangle *t);
+
+#endif
